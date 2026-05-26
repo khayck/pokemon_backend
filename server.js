ff21ff344d6856e953e1ad33eae27c6bd572aeb2
khayck <khayck.fronza@escola.pr.gov.br>
2026-05-26 11:33:12 -0300

    atualizacao

--- server.js
+++ server.js
@@ -1,7 +1,9 @@
 const express = require('express');
+const cors = require('cors')
 const app = express();
 const porta = 8080;
 
+app.use(cors())
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
